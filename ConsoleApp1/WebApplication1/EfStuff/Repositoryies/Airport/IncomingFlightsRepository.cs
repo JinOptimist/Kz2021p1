a3357be0dbbd121af30d5a8baab4170443acb081
Kirill <perevyshink_1@mail.ru>
2021-03-31 22:30:39 +0600

    Added methods to populate IncomingFlights and DepartingFlights tables

--- ConsoleApp1/WebApplication1/EfStuff/Repositoryies/Airport/IncomingFlightsRepository.cs
+++ ConsoleApp1/WebApplication1/EfStuff/Repositoryies/Airport/IncomingFlightsRepository.cs
@@ -13,5 +13,32 @@ namespace WebApplication1.EfStuff.Repositoryies.Airport
         public IncomingFlightsRepository(KzDbContext kzDbContext) : base(kzDbContext)
         {
         }
+        public void PopulateIncomingFlights()
+        {
+            Random random = new Random();
+            var rowsInc = from o in _kzDbContext.IncomingFlightsInfo select o;
+            foreach (var row in rowsInc)
+            {
+                Remove(row);
+            }
+            string[] FlightsIdPool = new[] { "DV 701", "KC 671", "DV 703", "IQ 354", "IQ 408", "KC 7051" };
+            string[] PlasesPool = new[] { "Dublin", "Moscow", "New-York", "London", "Tokyo", "Paris" };
+            string[] IncomingStatusesPool = new[] { "Landed", "Expected", "Delayed" };
+            string[] DepartingStatusesPool = new[] { "Canceled", "On Time", "Delayed", "Departed" };
+            Console.WriteLine("Creating incoming flights...");
+            for (int i = 0; i < random.Next(5, 10); i++)
+            {
+                IncomingFlightInfo incomingFlight = new IncomingFlightInfo
+                {
+                    FlightId = FlightsIdPool[random.Next(FlightsIdPool.Length)],
+                    Origin = PlasesPool[random.Next(PlasesPool.Length)],
+                    SetDate = DateTime.Today.AddHours(random.Next(5)),
+                    Status = IncomingStatusesPool[random.Next(IncomingStatusesPool.Length)],
+                    Airline = "International Airlines",
+                };
+                Save(incomingFlight);
+            }
+            Console.WriteLine("Incoming flights are created");
+        }
     }
 }
