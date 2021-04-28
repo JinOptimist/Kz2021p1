a908c098f7a7baf31c9fda917243bf1c45b642be
Serik Serikbay <bsserikbay@gmail.com>
2021-04-28 16:13:44 +0600

    Add views

---
+++ ConsoleApp1/WebApplication1/Views/Elections/Archive.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Views.Elections
+{
+    public class Archive : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
