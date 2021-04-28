b93d46d5ffd8085417cdb0b6f4a67296b3050dc4
Serik Serikbay <bsserikbay@gmail.com>
2021-04-28 16:12:54 +0600

    Add migration

---
+++ ConsoleApp1/WebApplication1/Migrations/20210428100512_AddElection.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class AddElection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Elections",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Start = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    End = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Elections", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Candidates",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    AvatarUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Job = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Slogan = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<int>(type: "int", nullable: false),
+                    Idea = table.Column<int>(type: "int", nullable: false),
+                    CitizenId = table.Column<long>(type: "bigint", nullable: false),
+                    BallotId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Candidates_Citizens_CitizenId",
+                        column: x => x.CitizenId,
+                        principalTable: "Citizens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ballots",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ElectionId = table.Column<long>(type: "bigint", nullable: false),
+                    CitizenId = table.Column<long>(type: "bigint", nullable: false),
+                    CandidateId = table.Column<long>(type: "bigint", nullable: true),
+                    VoteTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ballots", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ballots_Candidates_CandidateId",
+                        column: x => x.CandidateId,
+                        principalTable: "Candidates",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Ballots_Citizens_CitizenId",
+                        column: x => x.CitizenId,
+                        principalTable: "Citizens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CandidateElection",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CandidateId = table.Column<long>(type: "bigint", nullable: false),
+                    ElectionId = table.Column<long>(type: "bigint", nullable: false),
+                    CandidateRegistrationTime = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CandidateElection", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CandidateElection_Candidates_CandidateId",
+                        column: x => x.CandidateId,
+                        principalTable: "Candidates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CandidateElection_Elections_ElectionId",
+                        column: x => x.ElectionId,
+                        principalTable: "Elections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ballots_CandidateId",
+                table: "Ballots",
+                column: "CandidateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ballots_CitizenId",
+                table: "Ballots",
+                column: "CitizenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CandidateElection_CandidateId",
+                table: "CandidateElection",
+                column: "CandidateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CandidateElection_ElectionId",
+                table: "CandidateElection",
+                column: "ElectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidates_BallotId",
+                table: "Candidates",
+                column: "BallotId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidates_CitizenId",
+                table: "Candidates",
+                column: "CitizenId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Candidates_Ballots_BallotId",
+                table: "Candidates",
+                column: "BallotId",
+                principalTable: "Ballots",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ballots_Candidates_CandidateId",
+                table: "Ballots");
+
+            migrationBuilder.DropTable(
+                name: "CandidateElection");
+
+            migrationBuilder.DropTable(
+                name: "Elections");
+
+            migrationBuilder.DropTable(
+                name: "Candidates");
+
+            migrationBuilder.DropTable(
+                name: "Ballots");
+        }
+    }
+}
